8f7dab1df44366e8765e95c5d256efe87ecce57c
namansharma24092007-rgb <namansharma24092007@gmail.com>
2026-09-23 18:01:41 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, request
+import sqlite3
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+# Database create/connect
+def init_db():
+    conn = sqlite3.connect("to_do.db")
+
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS todo (
+            SNO INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            desc TEXT NOT NULL,
+            date_created TEXT NOT NULL
+        )
+    """)
+
+    conn.commit()
+    conn.close()
+
+
+init_db()
+
+
+@app.route("/")
+def hi():
+    conn = sqlite3.connect("to_do.db")
+
+    todos = conn.execute(
+        "SELECT * FROM todo ORDER BY SNO DESC"
+    ).fetchall()
+
+    conn.close()
+
+    return render_template("index.html", todos=todos)
+
+
+
+@app.route("/add", methods=["POST"])
+def add_todo():
+    title = request.form["title"]
+    desc = request.form["desc"]
+
+    date_created = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    conn = sqlite3.connect("to_do.db")
+
+    conn.execute(
+        "INSERT INTO todo (title, desc, date_created) VALUES (?, ?, ?)",
+        (title, desc, date_created)
+    )
+
+    conn.commit()
+    conn.close()
+
+    return hi()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
